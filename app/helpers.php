02fdaa201dff57ef177acf2af6aa8a4da25e4d4c
Edward <78192184+F8TGT@users.noreply.github.com>
2025-09-09 08:13:17 -0600

    → Created a render function in the helper file to return the view and variables to the layout.

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,12 @@
 <?php
+
+function render($view, $data = [])
+{
+    extract($data);
+
+    ob_start();
+    require_once __DIR__."/views/".$view.".php";
+    $content = ob_get_clean();
+
+    require_once __DIR__."/views/layout.php";
+}
