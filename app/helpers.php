ed2d2df3936b930d31c97f4bf7a2e30961da0f78
Edward <78192184+F8TGT@users.noreply.github.com>
2025-09-15 14:49:00 -0600

    → Added a base url function.

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,19 @@
 <?php
 
+//  BASE URL
+function base_url($path = '')
+{
+    if(defined('BASE_URL')) {
+        return BASE_URL.ltrim($path, '/');
+    }
+
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443 ? "https://" : "http://";
+    $host = $_SERVER['HTTP_HOST'];
+    $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+
+    return $protocol.$host.$base.'/'.ltrim($path, '/');
+}
+
 function render($view, $data = [], $layout = 'layout')
 {
     extract($data);
