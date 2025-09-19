e3ae86b398e332edc0343cc31df783cb4371952c
Edward <78192184+F8TGT@users.noreply.github.com>
2025-09-19 08:30:40 -0600

    → Created the User Controller, register user route and add the route in the navbar.

--- app/views/partials/home/navbar.php
+++ app/views/partials/home/navbar.php
@@ -19,7 +19,8 @@
             <a class="nav-link" href="login.html">Login</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="register.html">Register</a>
+            <a class="nav-link" href="<?php
+            echo base_url('user/register'); ?>">Register</a>
         </li>
     </ul>
 </nav>
