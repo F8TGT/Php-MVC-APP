4474dbf10e242bf14a9180c152f100b14efbc8fe
Edward <78192184+F8TGT@users.noreply.github.com>
2025-09-24 08:55:47 -0600

    → Created the DB and added the configurations to the config file.

--- config/config.php
+++ config/config.php
@@ -1,3 +1,16 @@
 <?php
 
-const BASE_URL = "http://mvc-app.local/";
+return [
+    'database' => [
+        'host' => 'localhost',
+        'database' => 'mvc_app_db',
+        'username' => 'root',
+        'password' => '',
+        'port' => 3307,
+        'charset' => 'utf8mb4',
+    ],
+    'app' => [
+        'base_url' => 'http://mvc-app.local/',
+        'debug' => true,
+    ],
+];
