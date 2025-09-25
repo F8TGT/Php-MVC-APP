7fbb2bdb5a45413aadea92bc5d880d1786a4ccd4
Edward <78192184+F8TGT@users.noreply.github.com>
2025-09-25 08:31:08 -0600

    → Created a file for the database class.

--- config/database.php
+++ config/database.php
@@ -1,1 +1,10 @@
 <?php
+
+return [
+    'host' => 'localhost',
+    'database' => 'mvc_app_db',
+    'username' => 'root',
+    'password' => '',
+    'port' => 3307,
+    'charset' => 'utf8mb4',
+];
