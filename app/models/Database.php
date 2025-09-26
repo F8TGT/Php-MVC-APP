776c565ee76249a17ca56d48c38af61c5194b441
Edward <78192184+F8TGT@users.noreply.github.com>
2025-09-26 08:11:52 -0600

    → Created a constructor for the database class.

--- app/models/Database.php
+++ app/models/Database.php
@@ -1,5 +1,16 @@
 <?php
 
-class Database {
-
+class Database
+{
+    public function __contruct()
+    {
+        $config = require base_path('config/config.php');
+        $dbConfig = $config['database'];
+        $host = $dbConfig['host'];
+        $dbname = $dbConfig['database'];
+        $username = $dbConfig['username'];
+        $password = $dbConfig['password'];
+        $port = $dbConfig['port'];
+        $charset = $dbConfig['charset'];
+    }
 }
